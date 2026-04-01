d870eedf316211a3335ee2442f7515f2f54eb3fe
Hong, Jaemin <jammin8507@gmail.com>
2026-04-01 11:37:15 +0900

    five

--- src/World.java
+++ src/World.java
@@ -1,7 +1,10 @@
-public class Main {
+public class World {
 
 	public static void run() {
 		System.out.println("202417087");
 	}
 
+	public static void run2() {
+		System.out.println("hello world");
+	}
 }
