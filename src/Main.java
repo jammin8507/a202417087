72e1f1b7bb3d0f0e29aebe549d574523e4176e95
Hong, Jaemin <jammin8507@gmail.com>
2026-04-01 11:32:36 +0900

    three

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("홍재민");
-
+		World.run();
 	}
 
 }
---
+++ src/World.java
@@ -0,0 +1,7 @@
+public class Main {
+
+	public static void run() {
+		System.out.println("202417087");
+	}
+
+}
